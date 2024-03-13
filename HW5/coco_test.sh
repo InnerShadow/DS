38f8b1b51526f4a72647d079576896895be27e53
Nobody <mishasikolenko@mail.ru>
2024-03-13 20:36:36 +0300

    Fix test

--- HW5/coco_test.sh
+++ HW5/coco_test.sh
@@ -2,8 +2,7 @@
 
 source yolov5env/bin/activate
 
-wget -O video.mp4 https://www.dropbox.com/scl/fi/053yt3wl89nva8loxc1r9/video.mp4?rlkey=7c8d3x86hf9f24y6em90j70ie&dl=0
-sleep 20
+wget -O video.mp4 'https://www.dropbox.com/scl/fi/053yt3wl89nva8loxc1r9/video.mp4?rlkey=7c8d3x86hf9f24y6em90j70ie&dl=0'
 
 cd yolov5
 python3 detect.py --weights runs/train/exp1/weights/best.pt --img 640 --conf 0.25 --source ../video.mp4
