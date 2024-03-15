73ec033f334255caba4d1997d1a6ae381c76deb3
Nobody <mishasikolenko@mail.ru>
2024-03-15 21:33:41 +0300

    More fixes

--- HW5/resize_images.py
+++ HW5/resize_images.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-input_dir = 'my_images'
+input_dir = 'tets'
 
 
 output_dir = 'images'
@@ -10,7 +10,7 @@ if not os.path.exists(output_dir):
 
 
 for filename in os.listdir(input_dir):
-    if filename.endswith('.jpg'):
+    if filename.endswith('.png'):
         with Image.open(os.path.join(input_dir, filename)) as img:
             resized_img = img.resize((640, 640))
             resized_img.save(os.path.join(output_dir, filename))
